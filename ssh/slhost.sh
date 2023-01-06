d454e4604e2c5d23a5ae3a6614e5db40e239d128
kzero-comeback <121999836+kzero-comeback@users.noreply.github.com>
2023-01-07 06:58:18 +0700

    Add files via upload

--- ssh/slhost.sh
+++ ssh/slhost.sh
@@ -20,11 +20,11 @@ echo "$SUB_DOMAIN" >> /etc/v2ray/domain
 #
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 subsl=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-DOMAIN=mantapxsl.my.id
-SUB_DOMAIN=onichan-${sub}.mantapxsl.my.id
-NS_DOMAIN=zerosl-${sub}.mantapxsl.my.id
-CF_ID=slinfinity69@gmail.com
-CF_KEY=dd2c5e0313f122b3c1833471d469b1025f492
+DOMAIN=myserveronline.tech
+SUB_DOMAIN=${sub}myserveronline.tech
+NS_DOMAIN=zerosl-${sub}myserveronline.tech
+CF_ID=anggarisma49@gmail.com
+CF_KEY=6MIWKDw5CAJm-Uxml2Amq8jNV8M66D-JTSvNUSz4
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${SUB_DOMAIN}..."
