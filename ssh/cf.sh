997339418ab349c641e09f4d736f81a141b18118
kzero-comeback <121999836+kzero-comeback@users.noreply.github.com>
2023-01-07 15:17:36 +0700

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -25,11 +25,11 @@ fi
 clear
 apt install jq curl -y
 ###DOM#AIN=installer.my.id
-DOMAIN=slxray.cf
+DOMAIN=myserveronline.tech
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c6)
-SUB_DOMAIN=${sub}.slxray.cf
-CF_ID=slinfinity69@gmail.com
-CF_KEY=3dcb550a77cb0a64dacf205243e3ac550f9db
+SUB_DOMAIN=${sub}.myserveronline.tech
+CF_ID=anggarisma49@gmail.com
+CF_KEY=896479123cbb19c2a964c3484d10ba2cf943e
 ##SUB_DOMAIN=${sub}.installer.my.id
 ##CF_ID=senowahyu62@gmail.com
 ##CF_KEY=84570683099becbeb0f04d8086ce6f309c56c
